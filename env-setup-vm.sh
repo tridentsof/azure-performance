288419f3b1b9155f1e376b24629f1699243bbccf
Ubuntu <udacityadmin@udacif662000000.hrdoqi50ljcu3e0p44104tx44g.dx.internal.cloudapp.net>
2023-07-08 04:22:40 +0000

    update the env-setup file

--- env-setup-vm.sh
+++ env-setup-vm.sh
@@ -8,6 +8,12 @@ echo 'python 3.7 installed'
 echo 'installing pip...'
 sudo -H pip3 install --upgrade pip
 echo 'pip upgraded'
+echo 'setting up default python version to 3.7...'
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
+sudo update-alternatives --config python3
+echo 'update the default python3 completed'
+echo "current python3 version is $(python3 --version)"
 echo 'installing redis...'
 wget http://download/redis.io/release/redis-6.2.4.tar.gz
 tar xzf redis-6.2.4.tar.gz
